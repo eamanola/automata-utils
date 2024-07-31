85492d8964c67b64c32fb83fb62d8ce1cc87a64e
Ehsun Amanolahi <ehsun.amanolahi@gmail.com>
2024-07-31 15:25:51 +0300

    color strings

--- src/lib/utils/logger.js
+++ src/lib/utils/logger.js
@@ -1,12 +1,19 @@
 const { NODE_ENV } = require('../../config');
 
-const error = console.error.bind(console);
+const RED = 31;
+const YELLOW = 33;
+
+const colorStrings = (color) => (arg) => (
+  typeof arg === 'string' ? `\x1b[${color}m${arg}\x1b[0m` : arg
+);
+
+const error = (...args) => console.error.call(console, ...args.map(colorStrings(RED)));
 
 const info = NODE_ENV === 'test'
   ? () => null
   : console.info.bind(console);
 
-const warn = console.warn.bind(console);
+const warn = (...args) => console.warn.call(console, ...args.map(colorStrings(YELLOW)));
 
 const logger = {
   error,
