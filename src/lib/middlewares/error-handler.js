4b7e5c14acf8c528f3bf55f3b90c08a35b6e5415
Ehsun Amanolahi <ehsun.amanolahi@gmail.com>
2026-06-08 08:04:41 +0300

    log errors option

--- src/lib/middlewares/error-handler.js
+++ src/lib/middlewares/error-handler.js
@@ -1,16 +1,21 @@
 const { logger } = require('../utils');
 
-const errorHandler = (knownErrors, { defaultTo500 = true } = {}) => (err, req, res, next) => {
+const errorHandler = (
+  knownErrors,
+  { defaultTo500 = true, log = false, logFunc = logger.error } = {},
+) => (err, req, res, next) => {
   const { name, status, message } = err;
 
   const knownError = Object.values(knownErrors).some(({ name: n }) => name === n);
   if (knownError) {
     res.status(status).json({ message });
+    if (log) logFunc('known error:', err);
   } else if (defaultTo500) {
     res.status(500).json({ message: 'internal error' });
-    logger.error('unhandled error', err);
+    if (log) logFunc(log, '500 unhandled error:', err);
   } else {
     next(err);
+    if (log) logFunc(log, 'next unhandled error:', err);
   }
 };
 
